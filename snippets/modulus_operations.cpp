684cd5eef3a8d4dc67a5af3d05ae72369e105c6d
nimish <nimish4july1998@gmail.com>
2020-05-05 01:29:59 +0530

    added modulus NCR, factorial modulo and factorial modulo inverse and moudlar inverse

--- snippets/modulus_operations.cpp
+++ snippets/modulus_operations.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 
 #define M 1000000007
+const int mxN = 10000000;
+long long fac[1 + mxN], facinv[1 + mxN], iv[1 +mxN];
+// fac stores factorial modulo mod
+// factinv stores modular inverse of factorial modulo mod
+// iv stores moular inverse modulo mod
+
 
 // Exponentiation Modulo
 long long modexpo(long long x, long long n, long long m = M){
@@ -75,6 +81,42 @@ long long modInverseModPrime(long long b, long long m)
 }
 
 
+void precal()
+{
+
+    iv[1]=1;
+	for(int i=2; i <= mxN; ++i)
+		iv[i] = M -(M/i)*(iv[M%i])%M;
+
+
+    fac[0] = 1;
+    for(int i = 1; i<mxN; i++)
+        fac[i] = (fac[i-1]*i) % M;
+
+    facinv[mxN-1] = modexpo(fac[mxN-1], M-2);
+
+    for(int i = mxN-1; i>0; i--)
+        facinv[i-1] = (facinv[i]*i) % M;
+
+    return;
+}
+
+
+long long NCR(long long n, long long r, long long mod = M){
+    //Finds numbers of ways selecting r objects from n objects modulus mod
+    // before using this function is necessary to precalculate values outf
+    // fact and facinv (therefore precal must be used)
+
+    if(n < r) return 0;
+    // simply n! * 1/(r!)   *  1/(n - r)!
+    return fac[n] * facinv[r] % mod * facinv[n-r] % mod;
+}
+
+
+
 int main(){
+    precal();
+    cout << iv[52345] << endl;
+    cout << modInverseModPrime(52345, M) << endl;
     return 0;
 }
